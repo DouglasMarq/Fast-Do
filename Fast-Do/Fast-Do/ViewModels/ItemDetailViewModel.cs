626631ae0b21fd09db4cce967d4b7b4b89220793
DouglasMarq <douglas.marq.alves@outlook.com>
2019-08-05 20:40:21 -0300

    First commit

---
+++ Fast-Do/Fast-Do/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Fast_Do.Models;
+
+namespace Fast_Do.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
