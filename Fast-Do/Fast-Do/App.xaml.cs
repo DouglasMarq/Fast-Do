5c690b073728228de186498e30815257ad947281
DouglasMarq <douglas.marq.alves@outlook.com>
2019-09-14 16:42:21 -0300

    removed navigation page on menupage

--- Fast-Do/Fast-Do/App.xaml.cs
+++ Fast-Do/Fast-Do/App.xaml.cs
@@ -14,7 +14,7 @@ namespace Fast_Do
 #if DEBUG
             HotReloader.Current.Run(this);
 #endif
-            MainPage = new NavigationPage(new MainPage());
+            MainPage = new MainPage();
         }
 
         protected override void OnStart()
