ebe7be91c3490451b5fcf11664dfbb6404fa22ab
DouglasMarq <douglas.marq.alves@outlook.com>
2019-09-14 15:22:15 -0300

    custom button...

---
+++ Fast-Do/Fast-Do.Android/CustomButtonRenderer.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Fast_Do.Droid;
+using Fast_Do.Views.Custom;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomButton), typeof(CustomButtonRenderer))]
+namespace Fast_Do.Droid
+{
+    public class CustomButtonRenderer : ButtonRenderer
+    {
+        public CustomButtonRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Button> e)
+        {
+            base.OnElementChanged(e);
+            if (e.OldElement != null)
+            {
+                Control.Touch -= CustomButtonOnTouch;
+            }
+            if (e.NewElement != null)
+            {
+                Control.Touch += CustomButtonOnTouch;
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                Control.Touch -= CustomButtonOnTouch;
+            }
+            base.Dispose(disposing);
+        }
+
+        private void CustomButtonOnTouch(object sender, TouchEventArgs touchEventArgs)
+        {
+            if (touchEventArgs.Event.Action == MotionEventActions.Down)
+            {
+                Element.ScaleTo(0.8, 125, Easing.CubicOut);
+            }
+            else if (touchEventArgs.Event.Action == MotionEventActions.Up)
+            {
+                Element.ScaleTo(1, 125, Easing.CubicIn);
+
+                if (Element != null && Element.Command != null)
+                {
+                    Element.Command.Execute(Element.CommandParameter);
+                }
+            }
+        }
+    }
+}
---
+++ Fast-Do/Fast-Do/Views/Custom/CustomButton.cs
@@ -0,0 +1,9 @@
+﻿
+using Xamarin.Forms;
+
+namespace Fast_Do.Views.Custom
+{
+    public class CustomButton : Button
+    {
+    }
+}
